e633b0a5337f132799b9663d023c44ea93814e15
Radek Dabrowski <Radek44@users.noreply.github.com>
2023-04-02 07:56:18 -0700

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,70 @@
+function loadSymbols() {
+  const alphabetContainer = document.getElementById("alphabet-container");
+
+  fetch('json/symbols_data.json')
+    .then(response => response.json())
+    .then(symbolsData => {
+      symbolsData.forEach(symbol => {
+        const symbolContainer = document.createElement("div");
+        symbolContainer.className = "symbol-container";
+
+        // Load the SVG from the file
+        fetch(symbol.fileName)
+          .then(response => response.text())
+          .then(svgData => {
+            const svgContainer = document.createElement("div");
+            svgContainer.className = "svg-container";
+            svgContainer.innerHTML = svgData.trim();
+            symbolContainer.appendChild(svgContainer);
+
+            const symbolInfo = document.createElement("div");
+            symbolInfo.className = "symbol-info";
+
+            symbolInfo.innerHTML = `<strong>${symbol.name}</strong> - ${symbol.description}<br>IPA: ${symbol.ipa}<br>`;
+
+            // Add this code to create a table for examples
+            const examplesTable = document.createElement("table");
+            const headerRow = document.createElement("tr");
+
+            const languageHeader = document.createElement("th");
+            languageHeader.innerText = "Language";
+            headerRow.appendChild(languageHeader);
+
+            const soundHeader = document.createElement("th");
+            soundHeader.innerText = "Sound";
+            headerRow.appendChild(soundHeader);
+
+            const wordHeader = document.createElement("th");
+            wordHeader.innerText = "Word";
+            headerRow.appendChild(wordHeader);
+
+            examplesTable.appendChild(headerRow);
+
+            Object.keys(symbol.examples).forEach(language => {
+              const exampleRow = document.createElement("tr");
+
+              const languageCell = document.createElement("td");
+              languageCell.innerText = language;
+              exampleRow.appendChild(languageCell);
+
+              const soundCell = document.createElement("td");
+              soundCell.innerText = symbol.examples[language].split(" ")[0];
+              exampleRow.appendChild(soundCell);
+
+              const wordCell = document.createElement("td");
+              wordCell.innerText = symbol.examples[language].split(" ")[2];
+              exampleRow.appendChild(wordCell);
+
+              examplesTable.appendChild(exampleRow);
+            });
+
+            symbolInfo.appendChild(examplesTable);
+
+            symbolContainer.appendChild(symbolInfo);
+            alphabetContainer.appendChild(symbolContainer);
+          });
+      });
+    });
+}
+
+loadSymbols();
